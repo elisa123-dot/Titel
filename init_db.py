061fedab4c4d023907bee3e854178a0301ffe126
Josephina Rauch <josephina1706@gmail.com>
2026-03-12 15:27:02 +0100

    Initialize database and add schema file

---
+++ init_db.py
@@ -0,0 +1,8 @@
+connection = sqlite3.connect('database.db')
+
+with open('schema.sql') as f:
+    connection.executescript(f.read())
+
+connection.commit()
+connection.close()
+print("Datenbank erfolgreich initialisiert!")
