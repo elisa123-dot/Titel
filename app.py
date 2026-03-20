5bdec31404dcd3f70cb768f67615fb67dfd8b2d0
Josephina Rauch <josephina1706@gmail.com>
2026-03-20 10:28:18 +0100

    Refactor app.py to remove JANEIN.html rendering and update response handling; modify index.html layout

--- app.py
+++ app.py
@@ -7,12 +7,11 @@ def index():
     if request.method == 'POST':
         Eingabe = request.form.get('Eingabe')
         if int(Eingabe) == 884900:
-        if int(Eingabe) == 884900:
             return render_template("indexcopy.html", "richtig", richtantwort="Antwort: 884900", deinantwort= "deine Antwort: ", eeingabe= int(Eingabe))
         else :
             return render_template("indexcopy.html", antwort= round(int(Eingabe)*100/884900,2), prozent="%", richtantwort="Antwort: 884900", deinantwort= "deine Antwort: ", eeingabe= int(Eingabe) )     
 
-    return render_template('JANEIN.html')
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
