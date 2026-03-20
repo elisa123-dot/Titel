9db4fe155243e4821596465807effbccfea01963
Josephina Rauch <josephina1706@gmail.com>
2026-03-20 11:30:55 +0100

    Refactor response handling in app.py and update templates for improved user interaction

--- app.py
+++ app.py
@@ -5,11 +5,24 @@ app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        Eingabe = request.form.get('Eingabe')
-        if int(Eingabe) == 884900:
-            return render_template("indexcopy.html", "richtig", richtantwort="Antwort: 884900", deinantwort= "deine Antwort: ", eeingabe= int(Eingabe))
+        jane = request.form.get('jane') 
+        if jane == "Nein":
+            return render_template("indexxcopy.html", Frage= "Wie hoch ist der Mount Everest (in cm)?")
+            Eingabe=request.form.get('eingabe')
+            if int(Eingabe) == 884900:
+                return render_template("indexcopy.html", "richtig", richtantwort="Antwort:",o=884900 ,deinantwort= "deine Antwort: ", eeingabe= int(Eingabe))
+            else :
+                return render_template("indexcopy.html", antwort= round(int(Eingabe)*100/884900,2), prozent="%", richtantwort="Antwort: 884900", o=8840900 ,deinantwort= "deine Antwort: ", eeingabe= int(Eingabe) ) 
         else :
-            return render_template("indexcopy.html", antwort= round(int(Eingabe)*100/884900,2), prozent="%", richtantwort="Antwort: 884900", deinantwort= "deine Antwort: ", eeingabe= int(Eingabe) )     
+            Eingabee=request.form.get('Eingabee')
+            return render_template("indexx.html", Frage= Eingabee )
+            Antwort=request.form.get('Antwort')
+            if int(Eingabe) == Antwort:
+                return render_template("indexcopy.html", "richtig", richtantwort="Antwort:", deinantwort= "deine Antwort: ", o=Antwort , eeingabe= int(Eingabe))
+            else :
+                return render_template("indexcopy.html", antwort= round(int(Eingabe)*100/884900,2), prozent="%", richtantwort="Antwort: ",o=Antwort ,deinantwort= "deine Antwort: ", eeingabe= int(Eingabe) ) 
+
+
 
     return render_template('index.html')
 
